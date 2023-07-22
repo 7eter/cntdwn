2474fd709fc36aac98cb1bcf4bb1476be2bd33d2
Peter <peteramried@web.de>
2023-07-22 10:36:20 +0200

    PWA installable

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+window.onload = () => {
+    'use strict';
+  
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+               .register('./sw.js');
+    }
+  }
+
 function insertBlank(n){
     if (n<10)
         return "&nbsp;"
